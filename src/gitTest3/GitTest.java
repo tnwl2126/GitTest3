d818961f8d0927dff598314dbfc175bc04f64e36
tnwl2126 <51833394+tnwl2126@users.noreply.github.com>
2020-06-15 11:40:44 +0900

    update todo

--- src/gitTest3/GitTest.java
+++ src/gitTest3/GitTest.java
@@ -2,4 +2,14 @@ package gitTest3;
 
 public class GitTest {
 
+	public static void main(String[] args) {
+		
+		GitTest git = new GitTest();
+		
+		git.start();
+	}
+	
+	public void start() {
+		System.out.println("GitTest3¿‘¥œ¥Ÿ.");
+	}
 }
